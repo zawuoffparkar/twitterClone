0ab9e8562d08c1cbece42071efd4d9a8dd704116
zawuoff <fouwaz232@gmail.com>
2025-12-17 23:45:04 +0000

    fixed an issue with deleting a tweet, and just removed a lot of empty spaces

--- index.js
+++ index.js
@@ -1,142 +1,133 @@
-import { tweetsData } from './data.js'
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+import { tweetsData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
-document.addEventListener('click', function(e){
-    if(e.target.dataset.like){
-       handleLikeClick(e.target.dataset.like) 
-    }
-    else if(e.target.dataset.retweet){
-        handleRetweetClick(e.target.dataset.retweet)
-    }
-    else if(e.target.dataset.reply){
-        handleReplyClick(e.target.dataset.reply)
-    }
-    else if(e.target.id === 'tweet-btn'){
-        handleTweetBtnClick()
-    }
-    else if(e.target.dataset.deleteTweet){
-        handleDeleteClick(e.target.dataset.deleteTweet)
-        console.log(e.target.dataset.deleteTweet)
-    }
-    else if(e.target.dataset.replyTweet){
-        replyTweetBtnClick(e.target.dataset.replyTweet)
-    }
-})
- 
-function handleLikeClick(tweetId){ 
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+  } else if (e.target.dataset.deleteTweet) {
+    handleDeleteClick(e.target.dataset.deleteTweet);
+    console.log(e.target.dataset.deleteTweet);
+  } else if (e.target.dataset.replyTweet) {
+    replyTweetBtnClick(e.target.dataset.replyTweet);
+  }
+});
 
-    if (targetTweetObj.isLiked){
-        targetTweetObj.likes--
-    }
-    else{
-        targetTweetObj.likes++ 
-    }
-    targetTweetObj.isLiked = !targetTweetObj.isLiked
-    render()
+function handleLikeClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  } else {
+    targetTweetObj.likes++;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+  render();
 }
 
-function handleRetweetClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
-    
-    if(targetTweetObj.isRetweeted){
-        targetTweetObj.retweets--
+function handleRetweetClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--;
+  } else {
+    targetTweetObj.retweets++;
+  }
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+  render();
+}
+
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
+}
+
+function handleDeleteClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  // can use indexOf method for knwoing the index of a something in an array.
+  const targetTweetDelete = tweetsData.indexOf(targetTweetObj);
+  // console.log(targetTweetDelete)
+  // basically can use splice to remove something in an array at a specific index
+  tweetsData.splice(targetTweetDelete, 1);
+  render();
+}
+
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById("tweet-input");
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+    render();
+    tweetInput.value = "";
+  }
+}
+
+function replyTweetBtnClick(tweetId) {
+  const replyInput = document.getElementById(`reply-input-${tweetId}`);
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+  if (replyInput.value) {
+    targetTweetObj.replies.unshift({
+      handle: `@Scrimba`,
+      profilePic: `images/scrimbalogo.png`,
+      tweetText: replyInput.value,
+    });
+    render();
+  }
+
+  console.log(targetTweetObj.replies);
+  // render()
+}
+
+function getFeedHtml() {
+  let feedHtml = ``;
+
+  tweetsData.forEach(function (tweet) {
+    let likeIconClass = "";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
     }
-    else{
-        targetTweetObj.retweets++
-    }
-    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
-    render() 
-}
 
-function handleReplyClick(replyId){
-    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
-}
+    let retweetIconClass = "";
 
-function handleDeleteClick(tweetId){
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
-    
-    // can use indexOf method for knwoing the index of a something in an array.
-    // const targetTweetDelete = tweetsData.indexOf(targetTweetObj)
-    // console.log(targetTweetDelete)
-    // basically can use splice to remove something in an array at a specific index
-    tweetsData.splice(targetTweetObj, 1)
-    render()
-}
-
-function handleTweetBtnClick(){
-    const tweetInput = document.getElementById('tweet-input')
-
-    if(tweetInput.value){
-        tweetsData.unshift({
-            handle: `@Scrimba`,
-            profilePic: `images/scrimbalogo.png`,
-            likes: 0,
-            retweets: 0,
-            tweetText: tweetInput.value,
-            replies: [], 
-            isLiked: false,
-            isRetweeted: false,
-            uuid: uuidv4()
-        })
-    render()
-    tweetInput.value = ''
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted";
     }
 
-}
-
-function replyTweetBtnClick(tweetId){
-    const replyInput = document.getElementById(`reply-input-${tweetId}`)
-    const targetTweetObj = tweetsData.filter(function(tweet){
-        return tweet.uuid === tweetId
-    })[0]
-    if(replyInput.value){
-        targetTweetObj.replies.unshift({
-                handle: `@Scrimba`,
-                profilePic: `images/scrimbalogo.png`,
-                tweetText: replyInput.value,
-            })
-            render()
-    }
-    
-    console.log(targetTweetObj.replies)
-    // render()
-}
-
-function getFeedHtml(){
-    let feedHtml = ``
-    
-    tweetsData.forEach(function(tweet){
-        
-        let likeIconClass = ''
-        
-        if (tweet.isLiked){
-            likeIconClass = 'liked'
-        }
-        
-        let retweetIconClass = ''
-        
-        if (tweet.isRetweeted){
-            retweetIconClass = 'retweeted'
-        }
-        
-        let repliesHtml = ` 
+    let repliesHtml = `
         <div class="tweet-reply">
 			<img src="images/scrimbalogo.png" class="profile-pic">
 			<textarea placeholder="What's happening?" id="reply-input-${tweet.uuid}"></textarea>
 	    </div>
 		<button id="reply-btn" data-reply-tweet= "${tweet.uuid}">Reply</button>
-        `
-        
-        if(tweet.replies.length > 0){
-            tweet.replies.forEach(function(reply){
-                repliesHtml+=`
+        `;
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
 <div class="tweet-reply">
     <div class="tweet-inner">
         <img src="${reply.profilePic}" class="profile-pic">
@@ -147,12 +138,11 @@ function getFeedHtml(){
      </div>
 
 </div>
-`
-            })
-        }
-        
-          
-        feedHtml += `
+`;
+      });
+    }
+
+    feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
@@ -180,23 +170,22 @@ function getFeedHtml(){
                 </span>
                 <span class="tweet-detail">
                     <i class="fa-solid fa-trash-can" data-delete-tweet="${tweet.uuid}"></i>
-                    
+
                 </span>
-            </div>   
-        </div>            
+            </div>
+        </div>
     </div>
     <div class="hidden" id="replies-${tweet.uuid}">
         ${repliesHtml}
-    </div>   
+    </div>
 </div>
-`
-   })
-   return feedHtml 
+`;
+  });
+  return feedHtml;
 }
 
-function render(){
-    document.getElementById('feed').innerHTML = getFeedHtml()
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml();
 }
 
-render()
-
+render();
